09f4ba3c62df09963209288ac2f0c6876affa9f0
Vanka Sai Deepika <saideepikavanka2006@gmail.com>
2026-06-29 21:24:54 +0530

    Add files via upload

---
+++ Array/TwoSum.java
@@ -0,0 +1,28 @@
+/*
+Problem: Two Sum
+Platform: LeetCode
+Difficulty: Easy
+
+Time Complexity: O(n)
+Space Complexity: O(n)
+*/
+
+import java.util.*;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+
+            map.put(nums[i], i);
+        }
+
+        return new int[]{};
+    }
+}
